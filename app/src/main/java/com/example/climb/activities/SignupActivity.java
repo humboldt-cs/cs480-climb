64fdc7ac208a0d82d47adc1c60212c01bce60af5
Skye Gibney <skyejgibney@gmail.com>
2020-04-02 10:21:18 -0700

    Integrated Login and Signup activities, fixing some errors

---
+++ app/src/main/java/com/example/climb/activities/SignupActivity.java
@@ -0,0 +1,90 @@
+package com.example.climb.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.climb.R;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity
+{
+    public static final String TAG = "SignupActivity";
+
+    EditText etUsername;
+    EditText etDisplayName;
+    EditText etPassword;
+    EditText etConfirmPassword;
+    EditText etEmail;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // Attach private variables
+        etUsername = findViewById(R.id.etUsername);
+        etDisplayName = findViewById(R.id.etDisplayName);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Log.i(TAG,"onClick login button");
+                String username = etUsername.getText().toString();
+                String displayName = etDisplayName.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+
+                signUpUser(username, displayName, password, email);
+            }
+        });
+    }
+
+    private void signUpUser(String username, String displayName, String password, String email) {
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        //user.setdisplayName(displayName);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(com.parse.ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    goMainActivity();
+                    Toast.makeText(SignupActivity.this, "Success! Account created", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(SignupActivity.this, "Issue with signup!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }
+}
