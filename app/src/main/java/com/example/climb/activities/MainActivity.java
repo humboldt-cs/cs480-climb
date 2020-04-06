2464ab676e4d4794fb85a9943cdcd23c3a179555
mmmz21 <emmawilcox121@gmail.com>
2020-04-06 01:16:08 -0700

    Added basic profile page and started on transitions between fragments in MainActivity

---
+++ app/src/main/java/com/example/climb/activities/MainActivity.java
@@ -0,0 +1,4 @@
+package com.example.climb.activities;
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/example/climb/fragments/ProfileFragment.java
@@ -0,0 +1,62 @@
+package com.example.climb.activities;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.climb.R;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import static com.parse.Parse.getApplicationContext;
+
+public class ProfileActivity extends Fragment {
+    private ImageView profileImg;
+    private TextView displayName;
+    private TextView userBio;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.profile_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        profileImg = view.findViewById(R.id.profileImg);
+        ParseFile img = currentUser.getParseFile("profilePhoto");
+        if (img != null) {
+            String imageUrl = img.getUrl();
+            Glide.with(getApplicationContext()).load(imageUrl).into(profileImg);
+
+        }
+
+        displayName = view.findViewById(R.id.displayName);
+        String display_name = currentUser.getString("displayName");
+        displayName.setText(display_name);
+
+        userBio = view.findViewById(R.id.userBio);
+        String bio = currentUser.getString("bio");
+        userBio.setText(bio);
+
+    }
+
+}
